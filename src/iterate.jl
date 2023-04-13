64e72029f9b1c5bc633f70b35ddcb05fa99d61ab
Lucas ONDEL YANG <lucas.ondel@cnrs.fr>
2023-04-13 13:37:32 +0200

    added tokens iterator

---
+++ src/iterate.jl
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: CECILL-2.1
+
+Base.iterate(A::AbstractFST) = nstates(A) == 0 ? nothing : (α(A), α(A))
+
+function Base.iterate(A::AbstractFST, uₙ)
+    uₙ₊₁ = T(A)' * uₙ
+    nnz(uₙ) > 0 ? (uₙ₊₁, uₙ₊₁) : nothing
+end
+
