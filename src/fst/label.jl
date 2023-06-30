bf3b35dc692d2fe70895e037127c667e0f9f808d
Martin Kocour <martinkocour1102@gmail.com>
2023-06-30 10:24:16 +0200

    Remove label functions which are no longer usefull.

--- src/fst/label.jl
+++ src/fst/label.jl
@@ -30,8 +30,3 @@ hasinputepsilon(l::Mapping) = isepsilon(first(l))
 
 hasoutputepsilon(l::SymbolId) = isepsilon(l)
 hasoutputepsilon(l::Mapping) = isepsilon(last(l))
-
-inputsymbol(l::SymbolId) = l
-inputsymbol(l::Mapping) = first(l)
-outputsymbol(l::SymbolId) = l
-outputsymbol(l::Mapping) = last(l)
