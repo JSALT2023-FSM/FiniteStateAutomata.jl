eff222c8c9dbfe763682d7cda1a9d6a20af06fd0
Martin Kocour <martinkocour1102@gmail.com>
2023-06-30 18:33:09 +0200

    Working example of DenseFST from NN outputs

--- examples/FST Collection.jl
+++ examples/FST Collection.jl
@@ -7,10 +7,12 @@ using InteractiveUtils
 # ╔═╡ a183e6f2-157e-11ee-0d03-ad978a190e29
 begin
 	using Pkg
+	Pkg.activate("./env")
 	Pkg.develop(path="../")
 
 	using Revise
 	using FiniteStateAutomata
+	using NPZ
 end
 
 # ╔═╡ da6fdaa9-675d-49b8-80e7-a5aaae988b10
@@ -72,6 +74,45 @@ draw(
 	osymbols=symtables[:cyrillic]
 ) 
 
+# ╔═╡ 8b543c88-4136-4c77-935f-0e617f273a65
+md"""
+## Example 3: NN outputs as DenseFST
+"""
+
+# ╔═╡ f67183da-92b1-421b-834e-bd4f3dd815d4
+logits = NPZ.npzread("./assets/libri_examples/2830-3980-0002/logits.npy")
+
+# ╔═╡ a8a3c4ec-ca78-4b0e-ad8f-338b48f5c956
+begin
+	label_mapping = open("./assets/libri_examples/ctc_map.txt") do f
+		readlines(f)
+	end
+	label_mapping = Dict(k => v for (k,v) in enumerate(label_mapping))
+end
+
+# ╔═╡ 8dbc2622-22b6-49df-8e7e-3c7e2e10e01b
+begin
+	# lets represent the logits as label-dependent alignment lattice
+	local Q, L = size(logits)
+	Q += 1 # +1 for initial/final state
+	local K = LogSemiring{Float16, ℯ}
+	
+	local W = K.(logits)
+	local α = zeros(K, Q)
+	α[1] = one(K)
+	local ω = zeros(K, Q)
+	ω[end] = one(K)
+	local M = zeros(K, Q, Q, L, L)
+
+	for q in 1:(Q-1)
+		for l in 1:L
+			M[q, q+1, l, l] = W[q, l]
+ 		end
+	end
+	nn_fst = TensorFST(M, α, ω)
+	draw(nn_fst; isymbols=label_mapping, osymbols=label_mapping)
+end
+
 # ╔═╡ Cell order:
 # ╠═a183e6f2-157e-11ee-0d03-ad978a190e29
 # ╠═da6fdaa9-675d-49b8-80e7-a5aaae988b10
@@ -86,3 +127,7 @@ draw(
 # ╠═37d55fc1-dcdc-44d0-9a70-b3b4d7d796c5
 # ╠═8548092c-7902-4c38-b007-6799b6a346f8
 # ╠═3739bac6-b90e-444a-aa6f-03dd7f1e765b
+# ╟─8b543c88-4136-4c77-935f-0e617f273a65
+# ╠═f67183da-92b1-421b-834e-bd4f3dd815d4
+# ╠═a8a3c4ec-ca78-4b0e-ad8f-338b48f5c956
+# ╠═8dbc2622-22b6-49df-8e7e-3c7e2e10e01b
