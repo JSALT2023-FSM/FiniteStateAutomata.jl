d21f4ac795ea88742f27856939758218d6a22f7d
Lucas ONDEL YANG <lucas.ondel@cnrs.fr>
2023-07-05 15:30:43 +0200

    messy example

--- examples/scripts/1_createfst.jl
+++ examples/scripts/1_createfst.jl
@@ -1,10 +1,14 @@
 
 using ImageInTerminal
 using Sixel
+
+
 using FiniteStateAutomata
 
 # Declare the semiring we want to use.
 S = LogSemiring{Float32,1}
+
+symbols = open(loadsymbols, "../data/symboltables/emoticons.syms")
 
 # Compile a text-formatted FST
 fst1 = compile(
@@ -19,13 +23,34 @@ fst1 = compile(
 )
 
 println("1. FST compiled from a string")
-draw(fst1) |> dot(:png) |> rawdata -> display(MIME("image/png"), rawdata)
+draw(fst1; isymbols=symbols, osymbols=symbols) |> dot(:png) |> rawdata -> display(MIME("image/png"), rawdata)
+
+# Saving the dot format into a file.
+open("out.dot", "w") do f
+    write(f, draw(fst1))
+end
+
+# On the notebook
+# draw(fst1) |> dot(:svg) |> HTML
 
 println("2. FST compiled from a string compatible with OpenFST")
+fst2 = compile(
+    """
+    0 1 1 1 -1
+    1 1 2 2 0
+    1 2 3 3 1
+    0 2
+    1 3.4
+    """;
+    semiring=S,
+    openfst_compat=true
+)
+
+println("2. FST compiled from a string with 0-based index")
+draw(fst1) |> dot(:png) |> rawdata -> display(MIME("image/png"), rawdata)
 
 path = "../data/fsts/simple.txt"
 fst3 = open(path, "r") do f
-    # openfst_compat=true read the file as a 0-based state indexing.
     compile(f; semiring=S)
 end
 
